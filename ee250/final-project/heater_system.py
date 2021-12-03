8a4b83d1e591c8da646617748b289f4725c6cc53
Mark Bowen <mdbowen@usc.edu>
2021-12-03 04:51:53 -0800

    plug

--- ee250/final-project/heater_system.py
+++ ee250/final-project/heater_system.py
@@ -27,43 +27,43 @@ p = SmartPlug("192.168.1.169")
   #      await p.turn_off()
 async def main():
     while True:
-    try:
-        [ temp,hum ] = dht(dht_sensor_port,0)
-        print("temp =", temp, "C\thumidity =", hum,"%")
+        try:
+            [ temp,hum ] = dht(dht_sensor_port,0)
+            print("temp =", temp, "C\thumidity =", hum,"%")
 
-        if isnan(temp) is True or isnan(hum) is True:
-            raise TypeError('nan error')
+            if isnan(temp) is True or isnan(hum) is True:
+                raise TypeError('nan error')
 
-        await p.update()
-        if temp == 23 and p.is_on:
-            print(p.alias + "Is going to sleep")
-            await p.turn_off()
-        if temp < 23 and p.is_off:
-            print(p.alias + "Is waking up")
-            await p.turn_on()
-        #asyncio.run(main(temp))
-        #if temp < 23:
-         #   asyncio.run(on())
-        #if temp == 23:
-         #   asyncio.run(off())
-        t = str(temp)
-        h = str(hum)
+            await p.update()
+            if temp == 23 and p.is_on:
+                print(p.alias + "Is going to sleep")
+                await p.turn_off()
+            if temp < 23 and p.is_off:
+                print(p.alias + "Is waking up")
+                await p.turn_on()
+            #asyncio.run(main(temp))
+            #if temp < 23:
+            #   asyncio.run(on())
+            #if temp == 23:
+            #   asyncio.run(off())
+            t = str(temp)
+            h = str(hum)
 
-        # instead of inserting a bunch of whitespace, we can just insert a \n
-        # we're ensuring that if we get some strange strings on one line, the 2nd one won't be affected
-        setText_norefresh("Temp:" + t + "C\n" + "Humidity :" + h + "%")
+            # instead of inserting a bunch of whitespace, we can just insert a \n
+            # we're ensuring that if we get some strange strings on one line, the 2nd one won't be affected
+            setText_norefresh("Temp:" + t + "C\n" + "Humidity :" + h + "%")
 
-    except (IOError, TypeError) as e:
-        print(str(e))
-        setText("")
+        except (IOError, TypeError) as e:
+            print(str(e))
+            setText("")
 
-    except KeyboardInterrupt as e:
-        print(str(e))
-        setText("")
-        break
+        except KeyboardInterrupt as e:
+            print(str(e))
+            setText("")
+            break
 
-    # wait some time before re-updating the LCD
-    sleep(5)
+        # wait some time before re-updating the LCD
+        sleep(5)
 
 if __name__ == "__main__":
     asyncio.run(main())
