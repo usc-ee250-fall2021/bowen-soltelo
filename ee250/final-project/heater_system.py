1a454d4e1cf279144e1f00f42a6092ff0c2c8822
Mark Bowen <mdbowen@usc.edu>
2021-12-03 03:51:01 -0800

    plug

--- ee250/final-project/heater_system.py
+++ ee250/final-project/heater_system.py
@@ -8,32 +8,36 @@ dht_sensor_port = 7
 setRGB(0,128,0)
 p = SmartPlug("192.168.1.169")
 while True:
-	try:
-        # get the temperature and Humidity from the DHT sensor
-		[ temp,hum ] = dht(dht_sensor_port,0)
+    try:
+        [ temp,hum ] = dht(dht_sensor_port,0)
         print("temp =", temp, "C\thumidity =", hum,"%")
-        print("temp =", temp, "C\thumidity =", hum,"%")
-		if isnan(temp) is True or isnan(hum) is True:
-			raise TypeError('nan error')
 
-		t = str(temp)
-		h = str(hum)
+        if isnan(temp) is True or isnan(hum) is True:
+            raise TypeError('nan error')
+
+        if temp < 23:
+            await p.update()
+            print(p.alias + "Is waking up")
+            await p.turn_on()
+        if temp == 23:
+            await p.update()
+            print(p.alias + "Is going to sleep")
+            await p.turn_off()
+        t = str(temp)
+        h = str(hum)
+
         # instead of inserting a bunch of whitespace, we can just insert a \n
         # we're ensuring that if we get some strange strings on one line, the 2nd one won't be affected
-		setText_norefresh("Temp:" + t + "C\n" + "Humidity :" + h + "%")
+        setText_norefresh("Temp:" + t + "C\n" + "Humidity :" + h + "%")
 
-	except (IOError, TypeError) as e:
-		print(str(e))
-		# and since we got a type error
-		# then reset the LCD's text
-		setText("")
+    except (IOError, TypeError) as e:
+        print(str(e))
+        setText("")
 
-	except KeyboardInterrupt as e:
-		print(str(e))
-		# since we're exiting the program
-		# it's better to leave the LCD with a blank text
-		setText("")
-		break
+    except KeyboardInterrupt as e:
+        print(str(e))
+        setText("")
+        break
 
-	# wait some time before re-updating the LCD
-	sleep(5)
+    # wait some time before re-updating the LCD
+    sleep(5)
